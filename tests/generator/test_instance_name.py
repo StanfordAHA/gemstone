500f6de39f27dd9fcb62a598e333ee20dc161831
Raj Setaluri <raj.setaluri@gmail.com>
2019-02-13 23:09:48 -0800

    Add missing assert to instance name test

--- tests/generator/test_instance_name.py
+++ tests/generator/test_instance_name.py
@@ -10,5 +10,5 @@ def test_instance_name_tile():
     assert str(circuit.instances) == '[MuxWithDefaultWrapper_2_32_8_0_inst0 = MuxWithDefaultWrapper_2_32_8_0(), dummy_1 = ConfigRegister_32_8_32_0(name="dummy_1"), dummy_2 = ConfigRegister_32_8_32_1(name="dummy_2")]'  # noqa
     with tempfile.TemporaryDirectory() as tempdir:
         m.compile(f"{tempdir}/core", circuit)
-        check_files_equal(f"{tempdir}/core.v",
-                          "tests/generator/gold/core_instance_name.v")
+        assert check_files_equal(f"{tempdir}/core.v",
+                                 "tests/generator/gold/core_instance_name.v")
