459adfcf95ee9d8dbf6eabd6ae14041e354bbbd1
Raj Setaluri <raj.setaluri@gmail.com>
2019-03-11 14:22:05 -0700

    Wrap irun tcl script in class

--- gemstone/common/run_verilog_sim.py
+++ gemstone/common/run_verilog_sim.py
@@ -4,6 +4,19 @@ import tempfile
 from .util import deprecated
 
 
+class IrunCommand:
+    def __init__(self, outfile=None, run_ns=1000000):
+        self.outfile = "verilog.vcd" if outfile is None else outfile
+        self.run_ns = run_ns
+
+    def source(self):
+        return f"""
+database -open -vcd vcddb -into {self.outfile}.vcd -default -timescale ps
+probe -create -all -vcd -depth all
+run {int(self.run_ns)}ns
+quit"""
+
+
 def irun_available():
     return shutil.which("irun") is not None
 
@@ -21,23 +34,28 @@ def verilog_sim_available():
 
 # We don't cover this function because irun is not available on travis
 def irun(files,
-         tcl_file,
+         irun_cmd=None,
          top_name="top",
          cleanup=False):  # pragma: nocover
     if len(files) == 0:
         print("Warning: irun requires at least 1 input file. Skipping irun.")
         return True
+    if irun_cmd is None:
+        irun_cmd = IrunCommand()
     files_string = " ".join(files)
-    irun_cmd = f"irun -sv -top {top_name} -timescale 1ns/1ps -l irun.log " \
-               f"-access +rwc -notimingchecks -input {tcl_file} " \
-               f"{files_string}"
-    print(f"Running irun cmd: {irun_cmd}")
-    if not os.system(irun_cmd) == 0:
-        return False
-    if not cleanup:
-        return True
-    cleanup_cmd = "rm -rf verilog.vcd INCA_libs irun.*"
-    return os.system(cleanup_cmd) == 0
+    with tempfile.NamedTemporaryFile(delete=cleanup) as cmd_file:
+        with open(cmd_file, "w") as cmd_file_w:
+            cmd_file_w.write(irun_cmd.source())
+        irun_cmd = f"irun -sv -top {top_name} -timescale 1ns/1ps -l irun.log " \
+                   f"-access +rwc -notimingchecks -input {cmd_file} " \
+                   f"{files_string}"
+        print(f"Running irun cmd: {irun_cmd}")
+        if not os.system(irun_cmd) == 0:
+            return False
+        if not cleanup:
+            return True
+        cleanup_cmd = "rm -rf verilog.vcd INCA_libs irun.*"
+        return os.system(cleanup_cmd) == 0
 
 
 @deprecated("iverilog does not support system verilog")
