03f7068a97890c45cf349e1d7c7cdf031c27c2e9
Keyi Zhang <keyi@stanford.edu>
2019-04-20 18:05:20 -0700

    hot fix the bitvector syntax change

--- tests/common/test_genesis_wrapper.py
+++ tests/common/test_genesis_wrapper.py
@@ -79,6 +79,6 @@ def test_main(capsys):
     out, _ = capsys.readouterr()
     expected_out = f"""\
 Running genesis cmd 'Genesis2.pl -parse -generate -top test_run_genesis -input {''.join(INFILES)} -parameter test_run_genesis.width='{w}''
-test_run_genesis(clk: In(Bit), reset: In(Bit), in0: In(Bits({w})), in1: In(Bits({w})), sel: In(Bit), out: Out(Bits({w})))
+test_run_genesis(clk: In(Bit), reset: In(Bit), in0: In(Bits[{w}]), in1: In(Bits[{w}]), sel: In(Bit), out: Out(Bits[{w}]))
 """  # nopep8
     assert out == expected_out
