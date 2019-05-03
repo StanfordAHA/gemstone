4786eeb7484e8333f7c31e2e2e89ada1cfa820b4
Raj Setaluri <raj.setaluri@gmail.com>
2019-05-03 10:41:29 -0700

    Compile instance name test with coreir-verilog

--- tests/generator/test_instance_name.py
+++ tests/generator/test_instance_name.py
@@ -9,6 +9,6 @@ def test_instance_name_tile():
     circuit = core.circuit()
     assert str(circuit.instances) == '[MuxWithDefaultWrapper_2_32_8_0_inst0 = MuxWithDefaultWrapper_2_32_8_0(), dummy_1 = ConfigRegister_32_8_32_0(name="dummy_1"), dummy_2 = ConfigRegister_32_8_32_1(name="dummy_2")]'  # noqa
     with tempfile.TemporaryDirectory() as tempdir:
-        m.compile(f"{tempdir}/core", circuit)
+        m.compile(f"{tempdir}/core", circuit, output="coreir-verilog")
         assert check_files_equal(f"{tempdir}/core.v",
                                  "tests/generator/gold/core_instance_name.v")
