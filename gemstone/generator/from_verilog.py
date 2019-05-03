6a4d7ea842813ce1d5bfd380f9810962e312cf02
Raj Setaluri <raj.setaluri@gmail.com>
2019-05-03 09:00:00 -0700

    Make verilog wrapping always shallow

--- gemstone/generator/from_verilog.py
+++ gemstone/generator/from_verilog.py
@@ -5,5 +5,5 @@ from .from_magma import *
 class FromVerilog(FromMagma):
     def __init__(self, filename, target_modules=None, type_map={}):
         underlying = magma.DefineFromVerilogFile(filename, target_modules,
-                                                 type_map)[0]
+                                                 type_map, shallow=True)[0]
         super().__init__(underlying)
