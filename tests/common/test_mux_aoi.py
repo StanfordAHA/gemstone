a43011eb28f46848109b4d4c9cfd2208654fd353
Ankita Nayak <ankita0805@gmail.com>
2019-05-11 11:27:49 -0700

    update tests for new structure

--- tests/common/test_mux_aoi.py
+++ tests/common/test_mux_aoi.py
@@ -1,3 +1,7 @@
+import tempfile
+import glob
+import shutil
+import os
 import pytest
 from random import randint, choice
 import tempfile
@@ -33,7 +37,10 @@ def test_aoi_mux_wrapper(height, width):
         tester.poke(mux_circuit.S, BitVector[mux.sel_bits](i))
         tester.eval()
         tester.expect(mux_circuit.O, inputs[i])
+
     with tempfile.TemporaryDirectory() as tempdir:
+        for aoi_mux in glob.glob("gemstone/common/*.sv"):
+            shutil.copy(aoi_mux, tempdir)
         tester.compile_and_run(directory=tempdir,
                                magma_output="coreir-verilog",
                                flags=["-Wno-fatal"])
@@ -72,6 +79,44 @@ def test_aoi_const_mux_wrapper(height, width):
             tester.expect(mux_circuit.O, 0)
 
     with tempfile.TemporaryDirectory() as tempdir:
+        for aoi_mux in glob.glob("gemstone/common/*.sv"):
+            shutil.copy(aoi_mux, tempdir)
         tester.compile_and_run(directory=tempdir,
                                magma_output="coreir-verilog",
                                flags=["-Wno-fatal"])
+
+
+@pytest.mark.parametrize('height,width', [(choice([2, 4, 5, 6, 8, 9]),
+                                           randint(1, 32))
+                                          for _ in range(5)])
+#def test_aoi_const_X_prop_mux_wrapper(height, width):
+#    """
+#    Test that the mux wrapper circuit works as expected. Specifically, we
+#    initialize a mux with random height and width, and check that the output is
+#    as expected for select in range [0, height).
+#
+#    Note that we do not check the behavior with sel > height, because this is
+#    undefined behavior.
+#    """
+#    mux = AOIMuxWrapper(height, width, AOIMuxType.Const)
+#    assert mux.height == height
+#    assert mux.width == width
+#    assert mux.name() == \
+#        f"MuxWrapperAOI_{height}_{width}_{AOIMuxType.Const.name}"
+#
+#    mux_circuit = mux.circuit()
+#    tester = fault.Tester(mux_circuit)
+#    inputs = [fault.random.random_bv(width) for _ in range(height + 1)]
+#    for i, input_ in enumerate(inputs[:height]):
+#        tester.poke(mux_circuit.I[i], fault.UnknownValue)
+#    tester.poke(mux_circuit.S, height)
+#    tester.eval()
+#    tester.expect(mux_circuit.O, 0)
+#
+#    with tempfile.TemporaryDirectory() as tempdir:
+#        for aoi_mux in glob.glob("gemstone/common/*.sv"):
+#            shutil.copy(aoi_mux, tempdir)
+#        tester.compile_and_run(directory=tempdir,
+#                               magma_output="coreir-verilog",
+#                               flags=["-Wno-fatal"])
+#
