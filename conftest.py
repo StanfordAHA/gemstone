2ca57c8191119d0705c7c15627e1de3f31ba04f0
Keyi Zhang <keyi@stanford.edu>
2019-06-18 12:03:37 -0700

    update clear cache due to magma changes

--- conftest.py
+++ conftest.py
@@ -7,5 +7,5 @@ from gemstone.generator import clear_generator_cache
 @pytest.fixture(autouse=True)
 def magma_test():
     clear_cachedFunctions()
-    magma.backend.coreir_.__reset_context()
+    magma.backend.coreir_.CoreIRContextSingleton().reset_instance()
     clear_generator_cache()
