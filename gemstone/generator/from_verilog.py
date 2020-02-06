bf861869312252fc49c0376f7c86f261648eb385
rsetaluri <rsetaluri@users.noreply.github.com>
2020-02-06 14:38:13 -0800

    Revert "Fixes for upstream magma FromVerilog changes"

--- gemstone/generator/from_verilog.py
+++ gemstone/generator/from_verilog.py
@@ -4,7 +4,6 @@ from .from_magma import *
 
 class FromVerilog(FromMagma):
     def __init__(self, filename, target_modules=None, type_map={}):
-        underlying = magma.DefineFromVerilogFile(filename,
-                                                 target_modules=target_modules,
-                                                 type_map=type_map)[0]
+        underlying = magma.DefineFromVerilogFile(filename, target_modules,
+                                                 type_map, shallow=True)[0]
         super().__init__(underlying)
