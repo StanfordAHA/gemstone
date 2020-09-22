59a2da4ca2ae32a31ca9b861ac6b7451ed638734
Keyi Zhang <keyi@stanford.edu>
2020-09-22 15:28:19 -0700

    update the compress scheme with skips, which is required for SRAM config (lake); add some unit tests for the function

--- tests/common/test_util.py
+++ tests/common/test_util.py
@@ -1,6 +1,6 @@
 import tempfile
 import pathlib
-from gemstone.common.util import ip_available, deprecated
+from gemstone.common.util import ip_available, deprecated, compress_config_data
 
 
 def test_ip_available():
@@ -24,3 +24,24 @@ def test_deprecated():
     except RuntimeError as e:
         expected_msg = "Function foo is deprecated: foo is deprecated"
         assert e.__str__() == expected_msg
+
+
+def test_comparess_config_data():
+    # test out compressing bitstream
+    config_data = [(0, 1), (0, 2)]
+    config_data = compress_config_data(config_data)
+    assert len(config_data) == 1
+    assert config_data[0][0] == 0
+    assert config_data[0][1] == 1 | 2
+
+    # test out duplicate without zeroing out, which is required to get rom
+    # working under current lake design
+    config_data = [(0, 0), (0, 1), (0, 2),
+                   (1, 0), (1, 0), (1, 0)]
+    config_data = compress_config_data(config_data, skip_compression=[1])
+    assert len(config_data) == 1 + 3
+    assert config_data[0][0] == 0
+    assert config_data[0][1] == 1 | 2
+    for i in range(1, 4):
+        config_data[i][0] == 1
+        config_data[i][1] == 0
