c4f9649686703305bdef38f9cce446bc0bdc865c
Lenny Truong <lenny@stanford.edu>
2021-03-10 16:05:21 -0800

    update conftest API

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
 import pytest
 from magma import clear_cachedFunctions
-import magma.backend.coreir_
+import magma
 from gemstone.generator import clear_generator_cache
 
 
 @pytest.fixture(autouse=True)
 def magma_test():
     clear_cachedFunctions()
-    magma.backend.coreir_.CoreIRContextSingleton().reset_instance()
+    magma.frontend.coreir_.ResetCoreIR()
     clear_generator_cache()
