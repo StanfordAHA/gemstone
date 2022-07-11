988149e785880267cc472450b4e5f3e9853c003b
Keyi Zhang <keyi@stanford.edu>
2022-07-10 18:02:33 -0700

    fix broken test

--- tests/generator/test_instance_name.py
+++ tests/generator/test_instance_name.py
@@ -9,5 +9,5 @@ def test_instance_name_tile():
     core.finalize()
     circuit = core.circuit()
     print(str(circuit.instances))
-    assert str(circuit.instances) == '[config_reg_0 = ConfigRegister_32_8_32_0(name="config_reg_0"), dummy_1_inst0 = dummy_1(), config_reg_1 = ConfigRegister_32_8_32_1(name="config_reg_1"), dummy_2_inst0 = dummy_2(), mux_aoi_2_32_inst0 = mux_aoi_2_32()]'  # noqa
+    assert str(circuit.instances) == '[config_reg_0 = ConfigRegister_32_8_32_0(name="config_reg_0"), dummy_1_value = SliceWrapper_32_0_32(name="dummy_1_value"), config_reg_1 = ConfigRegister_32_8_32_1(name="config_reg_1"), dummy_2_value = SliceWrapper_32_0_32(name="dummy_2_value"), mux_aoi_2_32_inst0 = mux_aoi_2_32()]'  # noqa
 
